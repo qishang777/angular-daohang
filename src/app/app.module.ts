de7ef45b68c29978a8535ad695de88ecbf358a26
qishang777 <yangxiaodong3630@126.com>
2021-02-22 10:07:40 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,10 @@ import { ProductDetailsComponent } from "./product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: "", component: ProductListComponent }])
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent }
+    ])
   ],
   declarations: [
     AppComponent,
@@ -23,16 +26,6 @@ import { ProductDetailsComponent } from "./product-details/product-details.compo
     ProductDetailsComponent
   ],
   bootstrap: [AppComponent]
-})
-@NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: "", component: ProductListComponent },
-      { path: "products/:productId", component: ProductDetailsComponent }
-    ])
-  ]
 })
 export class AppModule {}
 
